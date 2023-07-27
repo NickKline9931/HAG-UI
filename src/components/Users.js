961d839f89c0413b89db89b99749fcdb3cd8020e
NickKline9931 <nickkline9931@gmail.com>
2023-07-27 16:26:53 -0400

    Add users component

---
+++ src/components/Users.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import userService from "../UserService";
+
+export default function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  function getUsers() {
+    userService.getUsers().then((response) => {
+      setUsers(response.data);
+    });
+  }
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Email</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.email}</td>
+              <td>{user.username}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
