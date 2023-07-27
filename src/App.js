543f5f8512eb3a03959920545e2c20da9410934b
NickKline9931 <nickkline9931@gmail.com>
2023-07-27 16:59:47 -0400

    Add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import Users from "./components/Users";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Users />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Users />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
