7bde3d016e84922eacbce80f4a9a8534dc4c967c
NickKline9931 <nickkline9931@gmail.com>
2023-08-01 16:02:35 -0400

    Add page parameter to browse route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import Browse from "./components/Browse";
 import Search from "./components/Search";
 import Artists from "./components/Artists";
+import RandomArtist from "./components/RandomArtist";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -12,9 +13,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="users" element={<Users />} />
-          <Route path="browse" element={<Browse />} />
+          <Route path="browse/:page" element={<Browse />} />
           <Route path="search" element={<Search />} />
           <Route path="artists" element={<Artists />} />
+          <Route path="randomartist" element={<RandomArtist />} />
         </Routes>
       </BrowserRouter>
     </div>
