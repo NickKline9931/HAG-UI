fb9ece4284d21ae4796e76e0d70cd4d822ca4b2e
NickKline9931 <nickkline9931@gmail.com>
2023-08-03 13:36:52 -0400

    import style sheets

--- src/components/ArtistPage.js
+++ src/components/ArtistPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import "../styles/ArtistPage.css";
 
 export default function ArtistPage() {
   const { id } = useParams();
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import "../styles/Search.css";
 
 export default function Search() {
   const navigate = useNavigate();
