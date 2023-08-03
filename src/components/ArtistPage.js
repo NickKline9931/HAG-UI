7ee951211dd17c946f129b5d0a49cc6b9094b736
NickKline9931 <nickkline9931@gmail.com>
2023-08-03 13:25:12 -0400

    Display artist's works

--- src/components/ArtistPage.js
+++ src/components/ArtistPage.js
@@ -4,6 +4,7 @@ import { useParams, useNavigate } from "react-router-dom";
 export default function ArtistPage() {
   const { id } = useParams();
   const [artist, setArtist] = useState({});
+  const [works, setWorks] = useState([]);
 
   useEffect(() => {
     getArtist();
@@ -11,6 +12,7 @@ export default function ArtistPage() {
 
   useEffect(() => {
     document.title = artist.displayname + " - Art Museum";
+    getWorks();
   }, [artist]);
 
   async function getArtist() {
@@ -23,20 +25,53 @@ export default function ArtistPage() {
       setArtist(artData);
     } else {
       const response = await fetch(
-        "https://api.harvardartmuseums.org/person?q=id:" +
+        "https://api.harvardartmuseums.org/person/" +
           id +
-          "&fields=displayname,id,url&apikey=929885c9-4f01-4b51-ab44-041662619591"
+          "?fields=displayname,id,url&apikey=929885c9-4f01-4b51-ab44-041662619591"
       );
       const data = await response.json();
-      const artData = data.records[0];
-      setArtist(artData);
+      setArtist(data);
     }
   }
+
+  async function getWorks() {
+    const response = await fetch(
+      "https://api.harvardartmuseums.org/object?person=" +
+        id +
+        "&size=20&page=1&fields=title,dated,primaryimageurl,baseimageurl,id,url&apikey=929885c9-4f01-4b51-ab44-041662619591"
+    );
+    const data = await response.json();
+    const artData = data.records;
+    setWorks(artData);
+  }
+
+  const workDisplay = works.map((work, index) => {
+    return (
+      <li>
+        <a href={work.url} target="_blank" rel="noopener noreferrer">
+          <img
+            src={work.primaryimageurl + "?height=150&width=150"}
+            className="browseImg"
+            style={{ backgroundImage: "url(" + work.baseimageurl + ")" }}
+            alt="work"
+          />
+        </a>
+        <h2>{work.title}</h2>
+        <h4>{work.dated}</h4>
+      </li>
+    );
+  });
+
   return (
     <div>
-      <a href={artist.url} target="_blank" rel="noopener noreferrer">
-        <h1>{artist.displayname}</h1>
-      </a>
+      <main>
+        <a href={artist.url} target="_blank" rel="noopener noreferrer">
+          <h1>{artist.displayname}</h1>
+        </a>
+        <div>
+          <ul>{workDisplay}</ul>
+        </div>
+      </main>
     </div>
   );
 }
