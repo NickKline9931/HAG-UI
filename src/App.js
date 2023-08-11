26be4e44264c031a5617d38e229b41a6a9634191
NickKline9931 <nickkline9931@gmail.com>
2023-08-11 18:45:42 -0400

    Add save work function

--- src/App.js
+++ src/App.js
@@ -6,8 +6,25 @@ import Artists from "./components/Artists";
 import ArtistPage from "./components/ArtistPage";
 import Register from "./components/Register";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import savedWorkService from "service/SavedWorkService";
 
 function App() {
+  function saveWork(work) {
+    const savedWork = {
+      id: work.id,
+      title: work.title,
+      primaryImageUrl: work.primaryimageurl,
+      baseImageUrl: work.baseimageurl,
+      dated: work.dated,
+      artist: work.people[0],
+      artistId: work.people[0].personid,
+      url: work.url,
+    };
+    savedWorkService.SaveWork(savedWork).then((response) => {
+      alert("Account Created");
+    });
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
